7ba4c2c8da39ee28ec40b6d50e57278ae8649ca0
Pavel <paveln0366@gmail.com>
2021-02-22 19:21:38 +0300

    Fixed incorrect input error

--- app/src/main/java/com/pavelpotapov/weather/MainActivity.java
+++ app/src/main/java/com/pavelpotapov/weather/MainActivity.java
@@ -2,14 +2,42 @@ package com.pavelpotapov.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    private final String WEATHER = "https://api.openweathermap.org/data/2.5/weather?q=%s&appid=7b7de927047026f2f798a4f49f92c528&lang=ru&units=metric";
+    private final String WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?q=%s&appid=7b7de927047026f2f798a4f49f92c528&lang=%s&units=%s";
+    private final static String WEATHER_ICON = "https://openweathermap.org/img/wn/%s@2x.png";
+
+    private String language;
+    private final String UNITS_METRIC = "metric";
+
     private EditText editTextCity;
-    private TextView textViewWeather;
+    private static TextView textViewWeather;
+    private static ImageView imageViewWeatherIcon;
+
+    private static String temperature;
+    private static String outside;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +45,107 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         editTextCity = findViewById(R.id.editTextCity);
         textViewWeather = findViewById(R.id.textViewWeather);
+        imageViewWeatherIcon = findViewById(R.id.imageViewWeatherIcon);
+        language = Locale.getDefault().getLanguage();
+        temperature = getString(R.string.temperature);
+        outside = getString(R.string.outside);
+    }
+
+    public void onClickShowWeather(View view) {
+        String city = editTextCity.getText().toString().trim();
+        if (!city.isEmpty()) {
+            DownloadWeatherTask task = new DownloadWeatherTask();
+            String url = String.format(WEATHER_URL, city, language, UNITS_METRIC);
+            try {
+                task.execute(url);
+            } catch (Exception e) {
+                Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private class DownloadWeatherTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            URL url = null;
+            HttpURLConnection urlConnection = null;
+            StringBuilder result = new StringBuilder();
+            try {
+                url = new URL(strings[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = urlConnection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader reader = new BufferedReader(inputStreamReader);
+                String line = reader.readLine();
+                while (line != null) {
+                    result.append(line);
+                    line = reader.readLine();
+                }
+                return result.toString();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try {
+                JSONObject jsonObject = new JSONObject(s);
+                String city = jsonObject.getString("name");
+                String temp = jsonObject.getJSONObject("main").getString("temp");
+                String description = jsonObject.getJSONArray("weather").getJSONObject(0).getString("description");
+                String weather = String.format("%s\n%s: %s\n%s: %s", city, temperature, temp, outside, description);
+                textViewWeather.setText(weather);
+
+                String icon = jsonObject.getJSONArray("weather").getJSONObject(0).getString("icon");
+                DownloadImageTask taskImage = new DownloadImageTask();
+                String urlImage = String.format(WEATHER_ICON, icon);
+                taskImage.execute(urlImage);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } catch (NullPointerException e) {
+                Toast.makeText(MainActivity.this, getString(R.string.correct_data), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private static class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            URL url = null;
+            HttpURLConnection urlConnection = null;
+            try {
+                url = new URL(strings[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = urlConnection.getInputStream();
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                return bitmap;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            imageViewWeatherIcon.setImageBitmap(bitmap);
+        }
     }
 }
